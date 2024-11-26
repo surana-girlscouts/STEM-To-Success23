f9c67cba410ca7f90800b8d791b876b6bc0a0c0c
surana3 <divishasurana71@gmail.com>
2024-11-26 01:27:17 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,193 @@
+/* If you're feeling fancy you can add interactivity 
+    to your site with Javascript */
+
+var button = $('.views');
+var header = $('.two');
+button.on('click', doSomethingone);
+
+ function doSomethingone() {
+   header.removeClass('hidden')
+ }
+
+var buttontwo = $('.culture')
+var headertwo = $('.three');
+buttontwo.on('click', doSomethingtwo);
+
+ function doSomethingtwo() {
+   headertwo.removeClass('hidden')
+ }
+
+var buttonthree = $('.views')
+var headerthree = $('.four');
+buttonthree.on('click', doSomethingthree);
+
+ function doSomethingthree() {
+   headerthree.removeClass('hidden')
+ }
+var buttonfour = $('.history')
+var headerfour = $('.five');
+buttonfour.on('click', doSomethingfour);
+
+ function doSomethingfour() {
+   headerfour.removeClass('hidden')
+ }
+
+var buttonfive = $('.history')
+var headerfive = $('.seven');
+buttonfive.on('click', doSomethingfive);
+
+ function doSomethingfive() {
+   headerfive.removeClass('hidden')
+ }
+
+var buttonsix = $('.water')
+var headersix = $('.six');
+buttonsix.on('click', doSomethingsix);
+
+ function doSomethingsix() {
+   headersix.removeClass('hidden')
+ }
+
+var buttonseven = $('.warm')
+var headerseven = $('.nine');
+buttonseven.on('click', doSomethingseven);
+
+ function doSomethingseven() {
+   headerseven.removeClass('hidden')
+ }
+
+var buttoneight = $('.cool')
+var headereight = $('.eight');
+buttoneight.on('click', doSomethingeight);
+
+ function doSomethingeight() {
+   headereight.removeClass('hidden')
+ }
+
+var buttonnine = $('.cool')
+var headernine = $('.ten');
+buttonnine.on('click', doSomethingnine);
+
+ function doSomethingnine() {
+   headernine.removeClass('hidden')
+ }
+
+var buttonten = $('.polarthree')
+var headerten = $('.eleven');
+buttonten.on('click', doSomethingten);
+
+ function doSomethingten() {
+   headerten.removeClass('hidden')
+ }
+
+var buttoneleven = $('.polarfour')
+var headereleven = $('.twelve');
+buttoneleven.on('click', doSomethingeleven);
+
+ function doSomethingeleven() {
+   headereleven.removeClass('hidden')
+ }
+
+var buttontwelve = $('.polarthree')
+var headertwelve = $('.thirteen');
+buttontwelve.on('click', doSomethingtwelve);
+
+ function doSomethingtwelve() {
+   headertwelve.removeClass('hidden')
+ }
+
+var buttonthirteen = $('.bfc')
+var headerthirteen = $('.fourteen');
+buttonthirteen.on('click', doSomethingthirteen);
+
+ function doSomethingthirteen() {
+   headerthirteen.removeClass('hidden')
+ }
+
+var buttonfourteen = $('.bfi')
+var headerfourteen = $('.fifteen');
+buttonfourteen.on('click', doSomethingfourteen);
+
+ function doSomethingfourteen() {
+   headerfourteen.removeClass('hidden')
+ }
+
+var buttonfifteen = $('.bfc')
+var headerfifteen = $('.sixteen');
+buttonfifteen.on('click', doSomethingfifteen);
+
+ function doSomethingfifteen() {
+   headerfifteen.removeClass('hidden')
+ }
+
+var buttonsixteen = $('.polarsix')
+var headersixteen = $('.seventeen');
+buttonsixteen.on('click', doSomethingsixteen);
+
+ function doSomethingsixteen() {
+   headersixteen.removeClass('hidden')
+ }
+
+var buttonseventeen = $('.polarfive')
+var headerseventeen = $('.eighteen');
+buttonseventeen.on('click', doSomethingseventeen);
+
+ function doSomethingseventeen() {
+   headerseventeen.removeClass('hidden')
+ }
+
+var buttoneighteen = $('.polarsix')
+var headereighteen = $('.nineteen');
+buttoneighteen.on('click', doSomethingeighteen);
+
+ function doSomethingeighteen() {
+   headereighteen.removeClass('hidden')
+ }
+
+var buttonnineteen = $('.phc')
+var headernineteen = $('.twenty');
+buttonnineteen.on('click', doSomethingnineteen);
+
+ function doSomethingnineteen() {
+   headernineteen.removeClass('hidden')
+ }
+
+var buttontwenty = $('.phi')
+var headertwenty = $('.twentyone');
+buttontwenty.on('click', doSomethingtwenty);
+
+ function doSomethingtwenty() {
+   headertwenty.removeClass('hidden')
+ }
+
+var buttontwentyone = $('.phc')
+var headertwentyone = $('.twentytwo');
+buttontwentyone.on('click', doSomethingtwentyone);
+
+ function doSomethingtwentyone() {
+   headertwentyone.removeClass('hidden')
+ }
+
+var buttontwentytwo = $('.polareight')
+var headertwentytwo = $('.twentyfour');
+buttontwentytwo.on('click', doSomethingtwentytwo);
+
+ function doSomethingtwentytwo() {
+   headertwentytwo.removeClass('hidden')
+ }
+
+var buttontwentythree = $('.polarseven')
+var headertwentythree = $('.twentythree');
+buttontwentythree.on('click', doSomethingtwentythree);
+
+ function doSomethingtwentythree() {
+   headertwentythree.removeClass('hidden')
+ }
+
+var buttontwentyfour = $('.polarseven')
+var headertwentyfour = $('.twentyfive');
+buttontwentyfour.on('click', doSomethingtwentyfour);
+
+ function doSomethingtwentyfour() {
+   headertwentyfour.removeClass('hidden')
+ }
